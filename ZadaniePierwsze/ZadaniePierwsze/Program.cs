b00556cc635d14c87f97d0bb0673a3b052775cad
s16707 <s16707@pjwstk.edu.pl>
2024-04-03 23:21:52 +0200

    Modyfikacja 2

--- ZadaniePierwsze/ZadaniePierwsze/Program.cs
+++ ZadaniePierwsze/ZadaniePierwsze/Program.cs
@@ -4,4 +4,7 @@ Console.WriteLine("Pierwszy commit");
 static void Main(string[] args)
 {
     Console.Write("Enter a Number : ");
+    
+    int number = int.Parse(Console.ReadLine());
+    bool IsPrime = true;
 }
