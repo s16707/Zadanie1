7c527e8b4b38db2e49ffcc833d590d6a0a8ae7d5
s16707 <s16707@pjwstk.edu.pl>
2024-04-04 00:01:45 +0200

    Statyczna metoda, która przyjmue tablicę int'ów i zwraca maksymalną wartość.

--- ZadaniePierwsze/ZadaniePierwsze/Program.cs
+++ ZadaniePierwsze/ZadaniePierwsze/Program.cs
@@ -1,19 +1,33 @@
-﻿
-Console.WriteLine("Pierwszy commit");
-
-static void Main(string[] args)
+﻿public class MathHelper
 {
-    Console.Write("Enter a Number : ");
-    
-    int number = int.Parse(Console.ReadLine());
-    bool IsPrime = true;
-    
-    for (int i = 2; i < number/2; i++)
+    public static int ZnajdzMaksimum(int[] tablica)
     {
-        if (number % i == 0)
+        if (tablica == null || tablica.Length == 0)
         {
-            IsPrime = false;
-            break;
+            throw new ArgumentException("Tablica jest pusta lub niepoprawna.");
         }
+
+        int maksimum = tablica[0]; // Zakładamy, że pierwszy element jest maksimum
+
+        // Iterujemy przez pozostałe elementy i aktualizujemy maksimum, jeśli jest większe
+        for (int i = 1; i < tablica.Length; i++)
+        {
+            if (tablica[i] > maksimum)
+            {
+                maksimum = tablica[i];
+            }
+        }
+
+        return maksimum;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] tablicaLiczb = { 1, 5, 3, 8, 2 };
+        int maksimum = MathHelper.ZnajdzMaksimum(tablicaLiczb);
+        Console.WriteLine("Maksimum: " + maksimum);
     }
 }
