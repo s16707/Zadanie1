7899f222a2db628b1633ba4bc95103ce5d435e1a
s16707 <s16707@pjwstk.edu.pl>
2024-04-03 23:23:14 +0200

    Modyfikacja 3

--- ZadaniePierwsze/ZadaniePierwsze/Program.cs
+++ ZadaniePierwsze/ZadaniePierwsze/Program.cs
@@ -7,4 +7,13 @@ static void Main(string[] args)
     
     int number = int.Parse(Console.ReadLine());
     bool IsPrime = true;
+    
+    for (int i = 2; i < number/2; i++)
+    {
+        if (number % i == 0)
+        {
+            IsPrime = false;
+            break;
+        }
+    }
 }
